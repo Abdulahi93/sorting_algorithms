651a900f8b8540d2a148ccda707ba8e887fda89a
Cabdalle <abnagsh@gmail.com>
2022-10-15 19:41:54 -0700

    done

---
+++ 3-quick_sort.c
@@ -0,0 +1,79 @@
+#include "sort.h"
+void quicksort_recursion(int array[], int low, int high, int size);
+int partition(int *array, int low, int high, size_t size);
+/**
+ * swap - swap values
+ * @x: value 1
+ * @y: value2
+ */
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+/**
+* quick_sort - sort array using quick sort algorithim
+* @array: array to be sorted
+* @size: size of the array
+*/
+void quick_sort(int *array, size_t size)
+{
+
+	quicksort_recursion(array, 0, (int)size - 1, (int) size);
+}
+/**
+ * quicksort_recursion - recursion function that do partion
+ * @low: value 1
+ * @high: value2
+ * @size: size of array
+ * @array: array
+ */
+void quicksort_recursion(int array[], int low, int high, int size)
+{
+	if (low < high)
+	{
+		int pivot_index = partition(array, low, high, (int) size);
+
+		quicksort_recursion(array, low, pivot_index - 1, (int) size);
+		quicksort_recursion(array, pivot_index + 1, high, (int) size);
+	}
+}
+/**
+ * partition - swap values
+ * @low: value 1
+ * @high: value2
+ * @size: size of array
+ * @array: array
+ * Return: the pivote value
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int i = (low - 1);
+	int pivot = array[high];
+	int j, tmp;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			tmp = array[j];
+			array[j] = array[i];
+			array[i] = tmp;
+			if (i != j)
+				print_array(array, size);
+		}
+	}
+
+	if (array[i + 1] != array[high])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = tmp;
+		if ((i + 1) != j)
+			print_array(array, size);
+	}
+
+	return (i + 1);
+}
---
+++ swap_.c
@@ -0,0 +1,24 @@
+#include <sort.h>
+/**
+ * swap_ - swap variables
+ * @a: first variable
+ * @b: second variable
+ * Return: 1 if swap is not made, 0 if swap is performed
+ */
+int swap_(int *array,int a, int b)
+{
+
+    if(*array[0] != *array[0])
+    {
+       int temp = *array[0];
+       *array[0] = *array[2];
+       *array[2] = temp;
+       return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+Footer
+© 2022 GitHub, Inc.
